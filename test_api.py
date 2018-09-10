b6905d41530807898f0c3f79de86f6f9053a03a2
Timothy Mubiru <mubirutimothy5@gmail.com>
2018-09-10 21:46:43 +0300

    test for post

--- test_api.py
+++ test_api.py
@@ -1,16 +1,24 @@
+""" Test class for project"""
 import unittest
 from api import app
 
 class IndexViewTest(unittest.TestCase):
-    
+    """ class defines test methods."""
+
     def setUp(self):
+        """ set default values for class. """
         self.app = app.test_client()
         self.app.testing = True
-    
+
     def test_index_page(self):
+        """ define test methods for index page. """
         index = self.app.get('/')
         self.assertIn('Home', str(index.data))
 
+    def test_create_order(self):
+        """ test post method """
+        post = self.app.post('/api/v1/orders', {"user_id": "3", "item": "chips", "quantity":"1"})
+        self.assertEqual(post.status_code, 201)
 
 if __name__ == "__main__":
     unittest.main()

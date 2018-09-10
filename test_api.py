c7106f588d78bb510fc02a84cf082c7e4b09271c
Timothy Mubiru <mubirutimothy5@gmail.com>
2018-09-10 22:07:07 +0300

    bug fix on test for post

--- test_api.py
+++ test_api.py
@@ -1,6 +1,7 @@
 """ Test class for project"""
 import unittest
 from api import app
+from flask import json
 
 class IndexViewTest(unittest.TestCase):
     """ class defines test methods."""
@@ -9,6 +10,7 @@ class IndexViewTest(unittest.TestCase):
         """ set default values for class. """
         self.app = app.test_client()
         self.app.testing = True
+        self.order = {"user_id": 3, "item": "chips", "quantity":1}
 
     def test_index_page(self):
         """ define test methods for index page. """
@@ -17,7 +19,8 @@ class IndexViewTest(unittest.TestCase):
 
     def test_create_order(self):
         """ test post method """
-        post = self.app.post('/api/v1/orders', {"user_id": "3", "item": "chips", "quantity":"1"})
+        # request = {"user_id": "3", "item": "chips", "quantity":"1"}
+        post = self.app.post('/api/v1/orders', data=json.dumps(self.order, ensure_ascii=False))
         self.assertEqual(post.status_code, 201)
 
 if __name__ == "__main__":

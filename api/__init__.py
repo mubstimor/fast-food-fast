c36ed3612a2b8ebf801ec979dcee0990d264b7f9
Timothy Mubiru <mubirutimothy5@gmail.com>
2018-09-25 10:55:18 +0300

    reverting to ui for improvement

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-""" Initialize the app """
-from flask import Flask
-
-app = Flask(__name__, instance_relative_config=True)
-
-from api import views
-# Load the config file
-app.config.from_object('config')
--- run.py
+++
@@ -1,5 +0,0 @@
-""" opens the app for running """
-from api import app
-
-if __name__ == '__main__':
-    app.run()

c823e1386c90d25f0f804fe53a32240d879a0f09
Timothy Mubiru <mubirutimothy5@gmail.com>
2018-09-27 21:43:10 +0300

    tweaking test values

--- api/tests/test_order.py
+++ api/tests/test_order.py
@@ -26,7 +26,7 @@ class OrderViewTest(unittest.TestCase):
             self.assertEqual(request.status_code, 201)
             self.assertEqual(request.headers['Content-Type'], 'application/json')
             self.assertEqual("chips", request.json['order']['item'])
-            self.assertEqual(1, request.json['order']['quantity'])
+            self.assertEqual(1, int(request.json['order']['quantity']))
 
     def test_create_order_without_item_in_request(self):
         """ test post method by not including item in request """
@@ -48,56 +48,42 @@ class OrderViewTest(unittest.TestCase):
 
     def test_retrieve_order(self):
         """ test fetch method """
-        # self.order['user_id'] = 7
-        # self.test_create_order()
-        request = self.app.get('/api/v1/orders/2')
+        request = self.app.get('/api/v1/orders/1')
         self.assertEqual(request.status_code, 200)
-        self.assertEqual(2, request.json['order']['id'])
+        self.assertEqual(1, request.json['order']['id'])
 
     def test_retrieve_unavailableorder(self):
         """ test fetch order method by passing an index that's not available """
-        # self.order['user_id'] = 8
-        # self.test_create_order()
         request = self.app.get('/api/v1/orders/89')
-        # self.assertEqual(request.status_code, 404)
         self.assertEqual("not found", request.json['order'])
 
     def test_get_all_orders(self):
         """ test get all orders method """
-        # self.order['user_id'] = 9
-        # self.test_create_order()
         request = self.app.get('/api/v1/orders')
         self.assertEqual(request.status_code, 200)
         self.assertGreater(len(request.json['orders']), 0)
 
     def test_update_order(self):
         """ test update method """
-        # self.order['user_id'] = 10
-        # self.test_create_order()
         request = self.app.put('/api/v1/orders/2', \
         json={"status":"accepted"})
         self.assertEqual(request.status_code, 200)
         print(request.json)
-        # self.assertEqual(2, request.json['order']['user_id'])
         self.assertEqual("accepted", request.json['order']['status'])
 
     def test_update_user_order(self):
         """ test update user order method """
-        # self.order['user_id'] = 2
-        # self.test_create_order()
-        request = self.app.put('api/v1/users/orders/2', \
-        json={"user_id": "7", "item": "Rice + Chapatti", "quantity":"4", "status":"pending"})
+        request = self.app.put('api/v1/users/orders/1', \
+        json={"user_id": "1", "item": "Rice + Chapatti", "quantity":"4", "status":"pending"})
         self.assertEqual(request.status_code, 200)
-        self.assertEqual(2, request.json['order']['id'])
+        self.assertEqual(1, request.json['order']['id'])
         self.assertEqual("Rice + Chapatti", request.json['order']['item'])
 
     def test_retrieve_user_order(self):
         """ test get user orders method """
-        # self.order['user_id'] = 1
-        # self.test_create_order()
-        request = self.app.get('/api/v1/users/myorders/8')
+        request = self.app.get('/api/v1/users/myorders/1')
         self.assertEqual(request.status_code, 200)
-        self.assertEqual(4, request.json['myorders'][0]['id'])
+        self.assertEqual(1, request.json['myorders'][0]['id'])
 
     def test_update_order_with_invalid_status_value(self):
         """ test update method by including a wrong status value """
@@ -113,7 +99,7 @@ class OrderViewTest(unittest.TestCase):
 
     def test_delete_unavailable_order(self):
         """ test delete method for an unavailable resource """
-        request = self.app.delete('/api/v1/orders/1')
+        request = self.app.delete('/api/v1/orders/20')
         # self.assertEqual(request.status_code, 404)
         self.assertEqual("unable to delete order", request.json['result'])
         

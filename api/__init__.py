c28f68c3711ec7e9394046d03a006f6e1ebeb6b3
Timothy Mubiru <mubirutimothy5@gmail.com>
2018-10-21 10:20:09 +0300

    ft(fastfoodfast): allow all headers
    set to allow all headers
    
    [Delivers #161203856]

--- api/__init__.py
+++ api/__init__.py
@@ -8,7 +8,7 @@ from api.db.database import DatabaseConnection
 
 app = Flask(__name__, instance_relative_config=True)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}},
-            allow_headers=["Content-Type", "Authorization"])
+            allow_headers="*")
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['CORS_HEADERS'] = 'Authorization'
 app.config['CORS_HEADERS'] = 'Origin'

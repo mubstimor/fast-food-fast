c4b92b3b1b86f3292cf55333a9ecdd1b53abae79
Timothy Mubiru <mubirutimothy5@gmail.com>
2018-10-23 16:19:18 +0300

    pep8 update function names to match snake case style

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -28,7 +28,7 @@ class UserViewTest(unittest.TestCase):
         self.assertEqual(request.status_code, 409)
         self.assertEqual("user already exists", request.json['message'])
 
-    def test_create_user_with_invalid_email(self):
+    def test_createuserwithinvalidemail(self):
         """ test post method by not including email in request """
         self.user['email'] = "john"
         request = self.app.post('/api/v1/auth/signup', json=self.user)
